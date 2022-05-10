08c89a6625d986b02573fa920c130260e809eac8
ancely <761090890@qq.com>
2022-05-10 17:26:34 +0800

    init c project

---
+++ cmake-build-debug/CMakeFiles/cstudy.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cstudy.dir/main.c.obj"
+  "cstudy.exe"
+  "cstudy.exe.manifest"
+  "cstudy.lib"
+  "cstudy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cstudy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
